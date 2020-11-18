85343254f7813d931d27a6eee7a597301e4c233c
Roman Maksimov <r.l.maksimov@gmail.com>
2020-11-19 00:35:16 +0300

    change cmdlet name to Invoke-SharpHound

--- SharpHound3/PowerShell Output/Template.ps1
+++ SharpHound3/PowerShell Output/Template.ps1
@@ -1,4 +1,4 @@
-﻿function Invoke-BloodHound{
+﻿function Invoke-SharpHound{
     <#
     .SYNOPSIS
 
@@ -202,29 +202,29 @@
 
     .EXAMPLE
 
-        PS C:\> Invoke-BloodHound
+        PS C:\> Invoke-SharpHound
 
         Executes the default collection options and exports JSONs to the current directory, compresses the data to a zip file,
         and then removes the JSON files from disk
 
     .EXAMPLE
-        
-        PS C:\> Invoke-BloodHound -Loop -LoopInterval 00:01:00 -LoopDuration 00:10:00
-    
+
+        PS C:\> Invoke-SharpHound -Loop -LoopInterval 00:01:00 -LoopDuration 00:10:00
+
         Executes session collection in a loop. Will wait 1 minute after each run to continue collection
         and will continue running for 10 minutes after which the script will exit
 
     .EXAMPLE
 
-        PS C:\> Invoke-BloodHound -CollectionMethod All
-    
+        PS C:\> Invoke-SharpHound -CollectionMethod All
+
         Runs ACL, ObjectProps, Container, and Default collection methods, compresses the data to a zip file,
         and then removes the JSON files from disk
 
     .EXAMPLE
 
-        PS C:\> Invoke-BloodHound -CollectionMethod DCOnly --NoSaveCache --RandomFilenames --EncryptZip
-    
+        PS C:\> Invoke-SharpHound -CollectionMethod DCOnly -NoSaveCache -RandomizeFilenames -EncryptZip
+
         (Opsec!) Run LDAP only collection methods (Groups, Trusts, ObjectProps, ACL, Containers, GPO Admins) without outputting the cache file to disk.
         Randomizes filenames of the JSON files and the zip file and adds a password to the zip file
     #>
